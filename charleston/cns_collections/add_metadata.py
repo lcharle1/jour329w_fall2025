deafdf6213035235f2d7e717bcc90764195b041a
lcharle1 <143475739+lcharle1@users.noreply.github.com>
2025-10-17 19:58:23 +0000

    CNS beat collection analysis

--- charleston/cns_collections/add_metadata.py
+++ charleston/cns_collections/add_metadata.py
@@ -66,10 +66,10 @@ def main():
       "people": ["Korean Americans", "Maryland immigrants", "President Trump"],
       "geographic_focus": ["United States","Maryland","Prince George's County"],
       "key_institutions": ["Maryland General Assembly", "Trump Administration"],
-      "reporter": ["who is the reporter(s) of the article"],
+      "reporter": ["who is the reporter(s)of the article"],
+
     }
     """
-
     # Process each story
     enhanced_stories = []
     for i, story in enumerate(stories):
